ffc08c24ec9661f2ae42fec496d7bfb25cea09dd
jondekerh <jondekerh@gmail.com>
2017-05-13 03:58:29 +0000

    updated with fixes - now works

--- public_html/euler4.js
+++ public_html/euler4.js
@@ -1,9 +1,10 @@
 function Palindrome(str) {
 
+str = str.toString();
 var len = Math.floor(str.length / 2);
 
   for (x = 0; x < len; x++)
-    if (str[x] !== str[str.length - x - 1])
+    if (str[x] != str[str.length - x - 1])
       return false;
   return true;
 }
@@ -13,14 +14,13 @@ function computeResult() {
 var answer = 0;
 
   for (i = 900; i <= 999; i++) {
-    for (k = 900; i <= 999; k++) {
-      (i*k); 
+    for (k = 900; k <= 999; k++) { 
       if (Palindrome(i*k)) {
         answer = (i*k);
       }
     }
   }
-  document.getElementById("OutputBox").innerHTML = sum;
+  document.getElementById("OutputBox").innerHTML = answer;
 }
 //palindrome check script i used:
 //http://stackoverflow.com/questions/14813369/palindrome-check-in-javascript
